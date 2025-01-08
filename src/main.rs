1b4902432f2276f44a086e4f02c4641a81e92c94
sri0711 <m.sathish0711@gmail.com>
2025-01-08 21:36:02 +0530

    new testing url added

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,9 @@ async fn download_end_point(
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(download_end_point));
+    let app = Router::new()
+        .route("/download", get(download_end_point))
+        .route("/", get("ok"));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();

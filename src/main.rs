7ccab67941aed0834fb8a1c91903f0c0f3b1758b
sri0711 <m.sathish0711@gmail.com>
2025-01-10 09:09:15 +0530

    try

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::path::Path;
 use tokio::fs;
 
 fn download_audio_from_youtube(url: String, output: &str) -> io::Result<()> {
-    let status = Command::new("yt-dlp --cookies ./cookies.json")
+    let status = Command::new("yt-dlp --user-agent \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\" --referer \"https://www.youtube.com\"")
         .arg("-x")
         .arg("--audio-format")
         .arg("m4a")

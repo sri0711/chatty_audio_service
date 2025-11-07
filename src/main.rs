67a2e6b2bcae20471b994754085dd3fc6053ffe4
sri0711 <m.sathish0711@gmail.com>
2025-11-08 00:55:29 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use chatty_audio::{app, helper::audio_codec::decrypt_url};
+use chatty_audio::{app /* helper::audio_codec::decrypt_url */};
 
 #[tokio::main]
 async fn main() {
-    let ciphertext = "ID2ieOjCrwfgWvL5sXl4B1ImC5QfbsDyboG4XrmNvTDrEDvrfcr0Mn/5QKRbmNP/KBfaFeyigA3duQuCdY/GmRw7tS9a8Gtq";
-    let decrypted = decrypt_url(ciphertext);
-    println!("{}", decrypted);
+    // let ciphertext = "ID2ieOjCrwfgWvL5sXl4B1ImC5QfbsDyboG4XrmNvTDrEDvrfcr0Mn/5QKRbmNP/KBfaFeyigA3duQuCdY/GmRw7tS9a8Gtq";
+    // let decrypted = decrypt_url(ciphertext);
+    // println!("{}", decrypted);
 
     std::panic::set_hook(Box::new(|info| {
         eprintln!("PANIC: {}", info);

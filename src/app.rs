dc28eba10f84ebbfa9ec9c2a35d363820ad7fbb5
sri0711 <m.sathish0711@gmail.com>
2025-11-08 00:57:58 +0530

    initial commit

--- src/app.rs
+++ src/app.rs
@@ -9,5 +9,5 @@ pub async fn run() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("server is started on http://localhost:3000/");
-    axum::serve(listener, route).await.unwrap()
+    axum::serve(listener, route.into_make_service()).await.unwrap()
 }
